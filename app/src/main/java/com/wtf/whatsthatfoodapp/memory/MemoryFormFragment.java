d83fce900a38143d2e683fa420d385a3b247ad0c
Alex Chew <alex@alexchew.net>
2017-03-10 13:06:51 -0800

    Fix price rating bar width in MemoryForm

--- app/src/main/java/com/wtf/whatsthatfoodapp/memory/MemoryFormFragment.java
+++ app/src/main/java/com/wtf/whatsthatfoodapp/memory/MemoryFormFragment.java
@@ -222,7 +222,7 @@ public class MemoryFormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_memory, container,
+        return inflater.inflate(R.layout.fragment_memory_form, container,
                 false);
     }
 
