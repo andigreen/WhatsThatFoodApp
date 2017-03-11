fc90c008a2ee5aa15562cb7acc0cb3d219e3a082
Peiran Li <pel047@ucsd.edu>
2017-03-10 21:25:21 -0800

    Fix SharePictureActivity

--- app/src/main/java/com/wtf/whatsthatfoodapp/share/SharePictureActivity.java
+++ app/src/main/java/com/wtf/whatsthatfoodapp/share/SharePictureActivity.java
@@ -1,62 +1,121 @@
 package com.wtf.whatsthatfoodapp.share;
 
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.StorageReference;
 import com.wtf.whatsthatfoodapp.R;
 import com.wtf.whatsthatfoodapp.auth.AuthUtils;
 import com.wtf.whatsthatfoodapp.memory.Memory;
 import com.wtf.whatsthatfoodapp.memory.MemoryDao;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class SharePictureActivity extends AppCompatActivity {
 
     Uri pictureUri;
     ImageView thumbnail;
+    Memory memory;
+    File localFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_picture);
 
-        Memory memory = getIntent().getParcelableExtra("memory");
+        memory = getIntent().getParcelableExtra("memory");
         thumbnail = (ImageView) findViewById(R.id.share_picture_thumbnail);
 
         MemoryDao dao = new MemoryDao(AuthUtils.getUserUid());
-        dao.getPhotoRef(memory).getDownloadUrl().addOnSuccessListener(
-                new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Glide.with(SharePictureActivity.this)
-                                .load(uri)
-                                .centerCrop()
-                                .dontAnimate() // required by CircleImageView
-                                .into(thumbnail);
-                        pictureUri = uri;
-                    }
-                });
+        final StorageReference photoRef = dao.getPhotoRef(memory);
+        try {
+            localFile = File.createTempFile("images", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        photoRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+                Bitmap bmImg = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                thumbnail.setImageBitmap(bmImg);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
 
 
         Button btn_share=(Button)findViewById(R.id.share_picture_share_button);
         btn_share.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                shareIt();
+                shareSingleImage(v);
             }
         });
+
+
     }
 
-    private void shareIt() {
-        //sharing implementation here
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("image/jpeg");
-        sharingIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
-        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(new ContextWrapper(this).getExternalFilesDir(null), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+    public void shareSingleImage(View view) {
+        Uri bmpUri = getLocalBitmapUri(thumbnail);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.putExtra(Intent.EXTRA_TEXT,"Hey, check this out!");
+            shareIntent.setType("*/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            // ...sharing failed, handle error
+            Toast.makeText(this,"Invalid Uri",Toast.LENGTH_LONG);
+        }
     }
 
 }
