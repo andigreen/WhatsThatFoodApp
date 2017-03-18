de070af952153f59f088aa7dcf42412877917d34
Peiran Li <pel047@ucsd.edu>
2017-03-17 18:15:19 -0700

    Remove unused packages

--- app/src/main/java/com/wtf/whatsthatfoodapp/auth/SettingsActivity.java
+++ app/src/main/java/com/wtf/whatsthatfoodapp/auth/SettingsActivity.java
@@ -1,34 +1,8 @@
 package com.wtf.whatsthatfoodapp.auth;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.annotation.NonNull;
-import android.support.v7.preference.Preference;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.UploadTask;
-import com.wtf.whatsthatfoodapp.BasicActivity;
-import com.wtf.whatsthatfoodapp.R;
-import com.wtf.whatsthatfoodapp.user.UserSettings;
-import com.wtf.whatsthatfoodapp.user.UserSettingsDao;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends PreferenceActivity {
 
